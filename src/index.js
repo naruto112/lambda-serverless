741451eea44ea2ba7c12bc55fdbda2de56d6711c
Renato Moschetta <renatorock3@hotmail.com>
2022-07-30 11:54:01 -0300

    envio de ajuste SNS

--- src/index.js
+++ src/index.js
@@ -1,36 +1,53 @@
 const AWS = require("aws-sdk");
-const sharp = require("sharp");
-const { basename, extname } = require("path");
+// const sharp = require("sharp");
+// const { basename, extname } = require("path");
 
-const S3 = new AWS.S3();
+// const S3 = new AWS.S3();
 
-exports.handler = async function ({ Records: records }, context) {
+exports.handler = async function (event, context) {
+  // const { Records: records } = event;
   try {
-    await Promise.all(
-      records.map(async (record) => {
-        const { key } = record.s3.object;
+    //   await Promise.all(
+    //     records.map(async (record) => {
+    //       const { key } = record.s3.object;
 
-        const image = await S3.getObject({
-          Bucket: process.env.bucket,
-          Key: key,
-        }).promise();
+    //       const image = await S3.getObject({
+    //         Bucket: process.env.bucket,
+    //         Key: key,
+    //       }).promise();
 
-        const optimized = await sharp(image.Body)
-          .resize(1280, 720, { fit: "inside", withoutEnlargement: true })
-          .toFormat("jpeg", { progressive: true, quality: 50 })
-          .toBuffer();
+    //       const optimized = await sharp(image.Body)
+    //         .resize(1280, 720, { fit: "inside", withoutEnlargement: true })
+    //         .toFormat("jpeg", { progressive: true, quality: 50 })
+    //         .toBuffer();
 
-        await S3.putObject({
-          Body: optimized,
-          Bucket: process.env.bucket,
-          ContentType: "image/png",
-          Key: `compressed/${basename(key, extname(key))}.png`,
-        }).promise();
-      })
-    );
+    //       await S3.putObject({
+    //         Body: optimized,
+    //         Bucket: process.env.bucket,
+    //         ContentType: "image/png",
+    //         Key: `compressed/${basename(key, extname(key))}.png`,
+    //       }).promise();
+    //     })
+    //   );
+    //   return {
+    //     statusCode: 200,
+    //     body: { ok: true },
+    //   };
+
+    var sns = new AWS.SNS();
+
+    const params = {
+      Message: "Olá email",
+      TopicArn: "arn:aws:sns:us-east-1:611069673104:SNSMail",
+    };
+
+    await sns.publish(params).promise();
+
     return {
-      statusCode: 301,
-      body: { ok: true },
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Executando...",
+      }),
     };
   } catch (err) {
     return err;
